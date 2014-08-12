bcf8206028ec1a4fd92958af75043100a55380cd
Jared Cugno <jcugno@gmail.com>
2014-08-12 10:41:31 -0700

    Update track.js

--- track.js
+++ track.js
@@ -1,4 +1,5 @@
 
 setTimeout(function() { 
+  console.log("Setting an image here");
 (new Image()).src = (location.protocol == "https:" ? "https:" : "http:") + "//bcn.schooldata.com/track?pageUrl=" + window.location.href + '&ref=' + document.referrer + '_=' + new Date().getTime();
 }, 5000);
